425851ab95711175eb29d4c092c83ea06afb4227
Joe Kirincic <jdkirincic@gmail.com>
2019-08-23 14:50:08 -0400

    yeeter-mc-skeeter.

--- www/datasource-info.js
+++ www/datasource-info.js
@@ -2,10 +2,9 @@ $(document).on('shiny:sessioninitialized', function(event) {
   
   alert('Session initialized. Fetching datasource info...');
   
-  tableau.extensions.initializeAsync();
-  
-  var dsh = tableau.extensions.dashboardContent.dashboard;
-  
-  Shiny.setInputValue("dsh_name", dsh.name);
+  tableau.extensions.initializeAsync().then(function() {
+    var dsh = tableau.extensions.dashboardContent.dashboard;
+    Shiny.setInputValue("dsh_name", dsh.name);
+  })
   
 });
