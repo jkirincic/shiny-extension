9ca8c816da767fd0bdb488037d71f5bf973c746f
Joe Kirincic <jdkirincic@gmail.com>
2019-08-26 00:48:47 -0400

    Some shit.

--- app.R
+++ app.R
@@ -8,22 +8,25 @@
 #
 
 library(shiny)
+library(shinyjs)
 library(tidyverse)
 library(lubridate)
 
+
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  
+  useShinyjs(),
   tags$script(src = "datasource-info.js"),
   tags$script(src = "tableau.extensions.1.latest.js"),
   
   titlePanel("Shiny Extension!"),
   
   sidebarLayout(
-    sidebarPanel(actionButton(inputId = "extension-api-init", label = "Rev up those fryers!")),
+    sidebarPanel(actionButton(inputId = "extension_api_init", label = "Start")),
   mainPanel(
     textOutput("dsh_name_display"),
-    dataTableOutput("table_output")
+    textOutput("rslt")
   )
 )
 )
@@ -31,14 +34,31 @@ ui <- fluidPage(
 # Define server logic.
 server <- function(input, output, session) {
   
+  thing <- eventReactive(
+    input$extensions_api_init, {
+      ds <- runjs(code = "tableau.extensions.initializeAsync().then(function(){
+  const dsh = tableau.extensions.dashboardContent.dashboard
+                  ds = dsh.worksheets[0].getDatasourcesAsync().then(function(datasources){
+                  datasources[0].getUnderlyingDatasources(maxRows = 10).then(function(datasource){
+                  return datasource._data
+                  })
+                  })
+    })")
+      ds
+  }, ignoreNULL = FALSE)
+  
+  output$rslt <- renderPrint({
+    if(is.null(thing)){
+      "Nothing here."
+    }else{
+      thing
+    }
+  })
+  
   output$dsh_name_display <- renderText({
     validate(
       need(input$dsh_name, "Something's whack, Jack. Summon the hounds.")
     )
-  })
-  
-  output$table_output <- renderDataTable({
-    input$data
   })
   
 }
