88cd3e691841b19d9d18dc4cbc11a4b8dfc90373
Joe Kirincic <jdkirincic@gmail.com>
2019-08-26 01:29:13 -0400

    shit

--- www/datasource-info.js
+++ www/datasource-info.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   // Hook up an event handler for the load button click.
   // Wait to initialize until the button is clicked.
-  $("#extension-api-init").click(function() {
+  $("#extension_api_init").click(function() {
 
     // Disable the button after it's been clicked
     $("#extension_api_init").prop('disabled', true);
@@ -10,10 +10,18 @@ $(document).ready(function() {
     tableau.extensions.initializeAsync().then(function() {
 
       // Initialization succeeded! Get the dashboard
-      var dashboard = tableau.extensions.dashboardContent.dashboard;
+      const dashboard = tableau.extensions.dashboardContent.dashboard;
+      var datasource = dashboard.worksheets[0].getDataSourcesAsync().then(function(datasource_promises){
+        return datasource_promises[0];
+      }).then(function(datasource_promise){
+        return datasource_promise.getUnderlyingDataAsync(maxRows = 5).then(function(datasource){
+          return datasource._data;
+        });
+      });
 
       // Display the name of dashboard in the UI
       $("#dsh_name_display").html("I'm running in a dashboard named <strong>" + dashboard.name + "</strong>");
+      $("#rslt").html("Here's the first five (5) rows of the datasource." + datasource + " ... ");
     }, function(err) {
 
       // something went wrong in initialization
