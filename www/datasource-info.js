be6f9eb485dac12029b233e9a5f906e881e5e26c
Joe Kirincic <jdkirincic@gmail.com>
2019-08-26 02:17:22 -0400

    wut

--- www/datasource-info.js
+++ www/datasource-info.js
@@ -13,19 +13,21 @@ $(document).ready(function() {
       
       // Initialization succeeded! Get the dashboard
       const dashboard = tableau.extensions.dashboardContent.dashboard;
-      var datasource = dashboard.worksheets[0].getDataSourcesAsync().then(function(datasource_promises){
+      dashboard.worksheets[0].getDataSourcesAsync().then(function(datasource_promises){
         return datasource_promises[0];
       }).then(function(datasource_promise){
         return datasource_promise.getUnderlyingDataAsync(maxRows = 5).then(function(datasource){
           payload.push(datasource._data);
         });
       });
-
+      
+      var ds = payload[0];
+      
       // Display the name of dashboard in the UI
       $("#dsh_name_display").html("I'm running in a dashboard named <strong>" + dashboard.name + "</strong>");
-      $("#rslt").html("Here's the first five (5) rows of the datasource." + datasource.toString() + " ... ");
+      $("#rslt").html("Here's the first five (5) rows of the datasource." + ds.toString() + " ... ");
       
-      Shiny.setInputValue("data", payload[0]);
+      Shiny.setInputValue("data", ds);
       
     }, function(err) {
 
